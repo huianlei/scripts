f98fa5fb1fbee94d3086276b36594b78ff8750a3
huianlei <32159353+huianlei@users.noreply.github.com>
2020-03-31 12:48:48 +0800

    Create init.bat

---
+++ win/init.bat
@@ -0,0 +1,13 @@
+@echo off 
+title init
+set base=%cd%
+
+set work_dir=%base%\gameserver
+echo create work_dir=%work_dir%
+if exist %work_dir% (
+	rd Build /S/Q
+)
+mkdir %work_dir%
+
+cd /d %work_dir%
+git clone 
