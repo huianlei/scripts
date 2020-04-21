1499096c3d830a5d57157be11ccaa5196d224b69
Anlei Hui <anlei.hui@puzzleplusgames.com>
2020-04-21 15:26:53 +0800

    mod shell

--- linux/run_docker_all.sh
+++ linux/run_docker_all.sh
@@ -94,7 +94,7 @@ echo "redis_host=${redis_host}"
 docker_name="docker-gameserver"
 cmd="docker run --name ${docker_name} --restart=always -itd \
 	-p 10022:22 \
-	-p 9001:9001 \
+	-p ${DOCKER_GS_PORT-9001}:9001 \
 	-e MYSQL_USER="root" \
 	-e MYSQL_HOST="$mysql_host" \
 	-e MYSQL_PASSWORD="123147" \
