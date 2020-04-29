77c7e3644e872014ad86637b67f5fdd8d15905d3
Anlei Hui <anlei.hui@puzzlepulsgames.com>
2020-04-29 23:59:23 +0800

    add bat

---
+++ win/installer/install_docker.bat
@@ -0,0 +1,30 @@
+@echo off 
+title install docker env
+:: set terminal utf-8
+chcp 65001
+
+cd /d D:\docker-antia\scripts\win\installer
+echo USERPROFILE=%USERPROFILE%
+set cur_dir=%cd%
+echo %cur_dir%
+echo check and enable Microsoft-Hyper-V
+DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V
+echo check and enable Microsoft-Hyper-V success.
+choice /t 1 /d y /n >nul
+
+DockerDesktopInstaller.exe
+echo [========================= install success =======================]
+
+if exist C:\Program Files\Docker\Docker (
+	echo [========================= Starting Docker =======================]
+	@start cmd /c "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+	@exit
+)
+set DockerDaemonConfDir=%USERPROFILE%\.docker
+if not exist %DockerDaemonConfDir% (
+	md %DockerDaemonConfDir%
+)
+echo "copy daemon.json to %DockerDaemonConfDir%"
+copy "daemon.json" "%DockerDaemonConfDir%\daemon.json" /Y
+choice /t 5 /d y /n >nul
+
