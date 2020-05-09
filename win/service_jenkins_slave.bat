f10986b2b3e59455bf986de11852082eb43aaef3
Anlei Hui <anlei.hui@puzzlepulsgames.com>
2020-05-10 02:04:36 +0800

    mod

--- win/service_jenkins_slave.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd "%~dp0"  
-title Install Jenkins Slave Service
-
-echo %cd%
-sc delete "JenkinsSlave"
-sc create "JenkinsSlave" binPath= "%cd%\jenkins_slave.bat" type=share start=auto displayname="Jenkins Slave Services"
-net start "JenkinsSlave"
-pause
